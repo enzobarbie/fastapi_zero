9c945d338c1240d04d35191ebe4e83eb58fadaa5
Enzo Barbi <enzofagundes123@gmail.com>
2025-05-14 17:11:30 -0300

    Aula 02 - Fundamentos da WEB

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,10 @@ def test_root_deve_retornar_hello_world():
 
     assert response.json() == {'message': 'Hello World!'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_html():
+    response = client.get('/html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1>Hello World!</h1>' in response.text
